2534ac3dd221a0a92e84a163374a82e52b738d16
Nicholas Reich <nick@schoolph.umass.edu>
2014-04-10 10:07:30 -0400

    update titanic script.

---
+++ class21_titanic/titanic3.R
@@ -0,0 +1,24 @@
+## logistic regression lab with titanic3 data
+## PUBHLTH 690NR, Spring 2014
+
+require(PASWR)
+data(titanic3)
+head(titanic3)
+qplot(age, survived, data=titanic3, color=pclass, 
+      geom=c("point","smooth"), se=FALSE) +
+        facet_grid(facets=.~sex) + 
+        ylim(0,1)
+
+m2 <- glm(survived ~ pclass*sex*age, data=titanic3, family=gaussian)
+m1 <- glm(survived ~ pclass*sex*age, data=titanic3, family=binomial)
+m1a <- glm(survived ~ pclass*sex*age, data=titanic3, family=binomial)
+plot(m1)
+plot(residuals.glm(m1, type="deviance"))
+plot(residuals.glm(m1, type="pearson"))
+
+t1 <- titanic3[complete.cases(titanic3[,c("survived", "pclass", "sex", "age")]),]
+t1$preds <- fitted(m1, type="response")
+
+qplot(age, preds, data=t1, color=pclass, geom=c("point")) +
+        facet_grid(facets=.~sex) + 
+        ylim(0,1)
